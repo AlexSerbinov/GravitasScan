bfede27532f76f3f2e6c07e712490f68e84daaab
AlexSerbinov <alex.serbinov.dci@gmail.com>
2024-03-29 17:54:42 +0200

    Proxy service rewrited to using redis

--- services/proxy.js
+++ services/proxy.js
@@ -3,13 +3,13 @@ const EventEmitter = require("node:events")
 const path = require("path")
 const redis = require("../lib/redis/redis/lib/redis")
 const filePath = path.join(__dirname, "allUsers", "allUsers40.json")
+const { getArchiveData } = require("../lib/redis/index")
 
 const protocol = $.params.PROTOCOL
 
 const configPath = $.params.configPath
 const config = require(`${process.cwd()}${configPath}`)
 const fetcher = new EventEmitter()
-Object.assign(process.env, config) // While we use DB it's a simplest method to export config to other modules. After switching to redis we can remove this line
 
 // We prepare redis here because only in this place we have config params. And we don't want to use global variables.
 await redis.prepare(config.REDIS_HOST, config.REDIS_PORT) // Check if needed
@@ -21,42 +21,19 @@ $.send("start", { message: "proxy started" })
 
 let isSending = false // Flag to indicate if sending is in progress
 
-////////////////////////////// dev block //////////////
-const db = require("../lib/db")
-const connectionChecker = require("../lib/services/connections")
-
-async function init() {
-  const checkDBConnection = await connectionChecker.checkDBConnection()
-  const checkDBArchive = await connectionChecker.checkDBArchive(protocol)
-}
-
-init().catch(e => {
-  console.error(e)
-  db.sequelize.close().then(() => process.exit(1))
-})
-
-const getArchiveOrSubgraphUsers = async protocol => {
-  const { archive_users, listener_users } = await db.UserArchive.findOne({ where: { protocol } })
-
-  const allUsers = [...Object.values(archive_users).flat(), ...Object.values(listener_users).flat()]
-  return allUsers
-}
-////////////////////////////// dev block //////////////
-
 /**
- * Create fetcher
+ * Handles the 'sendUsersToSubgraph' event by fetching non-blacklisted users
+ * and sending them to the subgraph in batches.
  */
-
-// Handler for sending users to subgraph in batches.
 fetcher.on("sendUsersToSubgraph", async () => {
   const nonBlacklistedUsers = await getNonBlacklistedUsers(protocol)
   await sendUsersToSubraphInBatches(nonBlacklistedUsers)
 })
 
 /**
- * Create listener
+ * Listener for 'drain' events to manage sending users to the subgraph.
+ * It keeps track of the time and count of drain events to ensure timely processing.
  */
-
 let lastDrainTime = Date.now() // Initialize lastDrainTime to the current time
 let drainEventCount = 0 // Counter for drain events
 
@@ -82,6 +59,10 @@ $.on("drain", data => {
   }
 })
 
+/**
+ * Triggered upon receiving a drain event, this function fetches non-blacklisted users
+ * and sends them to the subgraph in batches.
+ */
 const onDrain = async () => {
   isSending = true
   const nonBlacklistedUsers = await getNonBlacklistedUsers(protocol)
@@ -89,20 +70,12 @@ const onDrain = async () => {
   isSending = false
 }
 
-// Utility function to read and parse the user file.
-const getUserFromFile = async () => {
-  const items = await fs.readFile(filePath, "utf8")
-  const parsedItems = JSON.parse(items)
-  // Map over parsedItems to extract only the 'user' field from each object
-  const usersOnly = parsedItems.map(item => item.user)
-
-  return usersOnly
-}
-
-// New utility function to get non-blacklisted users.
+/**
+ * Retrieves non-blacklisted users for the given protocol.
+ * @param {string} protocol - The protocol identifier.
+ * @returns {Promise<Array<string>>} An array of non-blacklisted user addresses.
+ */
 const getNonBlacklistedUsers = async protocol => {
-  // const allUsersFile = await getUserFromFile() // dev
-  // console.log(allUsersFile)
   const allUsers = await getArchiveOrSubgraphUsers(protocol)
 
   const usersToCheck = allUsers.map(userInfo => userInfo.user)
@@ -112,16 +85,30 @@ const getNonBlacklistedUsers = async protocol => {
   const nonBlacklistedUsers = allUsers.filter((_, index) => checkBlacklistUsers[index] === 0).map(userInfo => userInfo.user) // Mapping to get only user addresses
   console.log(`nonBlacklistedUsers users length = ${nonBlacklistedUsers.length}`)
 
-  // console.log(nonBlacklistedUsers)
   return nonBlacklistedUsers
 }
 
-// Send users in batches.
+/**
+ * Fetches users from the archive or subgraph for the given protocol.
+ * @param {string} protocol - The protocol identifier.
+ * @returns {Promise<Array<string>>} An array of user addresses.
+ */
+const getArchiveOrSubgraphUsers = async protocol => {
+  const allUsers = await getArchiveData(protocol, "archive_users")
+  // Extract the arrays and flatten them into a single array
+  const users = Object.values(allUsers).flat()
+  return users
+}
+
+/**
+ * Sends the non-blacklisted users to the subgraph in batches.
+ * @param {Array<string>} nonBlacklistedUsers - An array of non-blacklisted user addresses.
+ */
 const sendUsersToSubraphInBatches = async nonBlacklistedUsers => {
   isSending = true // Set the sending flag to true at the beginning
   const batchSize = 20
   const totalBatches = Math.ceil(nonBlacklistedUsers.length / batchSize)
-  let batchesSent = 0 // Counter for sent batches
+  let batchesSent = 0
 
   for (let i = 0; i < nonBlacklistedUsers.length; i += batchSize) {
     // Use setImmediate for asynchronous sending
@@ -136,7 +123,6 @@ const sendUsersToSubraphInBatches = async nonBlacklistedUsers => {
       }
       $.send("sendUsersToSubgraph", batch)
 
-      // Increment the counter after each batch is sent
       batchesSent++
       // Check if all batches have been sent
       if (batchesSent === totalBatches) isSending = false // Reset the sending flag after the last iteration
@@ -144,10 +130,11 @@ const sendUsersToSubraphInBatches = async nonBlacklistedUsers => {
   }
 }
 
+/**
+ * Sets up a timer to trigger the drain event manually if not received within a specified timeout.
+ */
 let drainTimer
 let manualTriggerCount = 0
-
-// If we do not have drain event for time in SEND_WITHOUT_DRAIN_EVENT_TIMEOUT. We sent event by timeout
 const setupDrainTimer = () => {
   clearTimeout(drainTimer)
 
